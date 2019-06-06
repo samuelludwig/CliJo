d94dd03ad3b59ee186e60d029edae6170ec64578
Samuel Ludwig <samuelludwig74@yahoo.com>
2019-06-06 13:35:25 -0400

    Add set_home_directory function.

--- config/config.exs
+++ config/config.exs
@@ -20,6 +20,8 @@ use Mix.Config
 #
 #     config :logger, level: :info
 #
+config :clijo, Clijo.Config,
+[config_file: "./user_config.json"]
 
 # It is also possible to import configuration files, relative to this
 # directory. For example, you can emulate configuration per environment
