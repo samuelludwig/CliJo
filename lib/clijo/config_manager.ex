ff87fe15e5d20cc1566a226810062f97ec58b4fc
Samuel Ludwig <samuelludwig74@yahoo.com>
2019-06-07 06:07:43 -0400

    Refactor and add get_home_directory/0.

---
+++ lib/clijo/config_manager.ex
@@ -0,0 +1,48 @@
+defmodule Clijo.ConfigManager do
+  @moduledoc """
+  The responsibility of the Clijo.ConfigManager module is to handle
+  any work that relates to the users configuration of CliJo.
+  """
+  @moduledoc since: "June 7th, 2019"
+
+  @user_config_path "./config/user_config.json"
+
+  @doc """
+  Takes in a string and sets `path` as the home_directory variable in
+  `user_config.json`
+
+  Returns `{:ok, path}` if successful, returns `{:error, reason}` if unsuccessful.
+  """
+  @doc since: "June 6th, 2019"
+  def set_home_directory(path) do
+    if File.exists?(path) do
+      path = Path.expand(path)
+      write_to_file = &(File.write!(@user_config_path, &1))
+      # Update settings map and write to file
+      %{get_user_config() | "home_directory" => path}
+      |> Jason.encode!()
+      |> write_to_file.()
+
+      {:ok, path}
+    else
+      {:error, "#{path} does not exist"}
+    end
+  end
+
+  @doc """
+    Grabs the value of the `home_directory` variable in
+    `user_config.json`.
+  """
+  @doc since: "June 7th, 2019"
+  def get_home_directory() do
+    get_user_config()
+    |> Map.get("home_directory")
+  end
+
+  # Returns a map of the settings defined in `user_config.json`
+  defp get_user_config() do
+    @user_config_path
+    |> File.read!()
+    |> Jason.decode!()
+  end
+end
