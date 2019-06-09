59043b3a5a79382fb338c75ca6214459fd4f01eb
samuelludwig <samuelludwig74@yahoo.com>
2019-06-09 10:19:30 -0400

    Refactor and add display_daily_log/1 and edit_daily_log/3 functions.

--- lib/clijo/journal_manager.ex
+++ lib/clijo/journal_manager.ex
@@ -4,11 +4,19 @@ defmodule Clijo.JournalManager do
   any work that involves creating, editing, or deleting entries in
   CliJo.
   """
-  @doc since: "June 7th, 2019"
+  @moduledoc since: "June 7th, 2019"
 
   alias Clijo.ConfigManager
 
-  def new_monthly_log() do
+  # *
+  # TODO: Add code for failure conditions in appropriate functions.
+  # *
+
+  def make_monthly_log() do
+    # TODO
+  end
+
+  def display_monthly_log() do
     # TODO
   end
 
@@ -22,20 +30,12 @@ defmodule Clijo.JournalManager do
   """
   @doc since: "June 8th, 2019"
   def make_daily_log(log_name \\ nil) do
-    {:ok, path} = generate_directory()
-    date = Date.utc_today()
-    day = Integer.to_string(date.day)
-
-    filename =
-      if is_nil(log_name) do
-        Path.join(path, day) <> ".md"
-      else
-        Path.join(path, log_name) <> ".md"
-      end
+    {:ok, filename} = get_log_path(log_name)
 
     header_title =
       if is_nil(log_name) do
-        Date.to_string(date)
+        Date.utc_today()
+        |> Date.to_string()
       else
         log_name
       end
@@ -74,8 +74,49 @@ defmodule Clijo.JournalManager do
     {:ok, path}
   end
 
-  def edit_entry(log_name \\ nil) do
-    # TODO
+  @doc """
+  Writes the given daily log `log_name` to the terminal with prepended
+  line numbers.
+  """
+  @doc since: "June 9th, 2019"
+  def display_daily_log(log_name \\ nil) do
+    {:ok, path} = make_daily_log(log_name)
+    {:ok, contents} = File.read(path)
+
+    contents
+    |> String.split("\n", trim: false)
+    |> Enum.with_index(1)
+    |> Enum.map(fn {line, line_num} -> # HACK
+                   "#{line_num}#{cond do
+                                   line_num >= 1000 -> " " # All this
+                                   line_num >= 100 -> "  " # is done to
+                                   line_num >= 10 -> "   " # make sure the
+                                   true -> "    "          # text is properly
+                                 end}#{line}" end)         # alinged.
+    |> Enum.each(&IO.puts(&1))
+
+    IO.puts("----------")
+  end
+
+  @doc """
+  Replaces content on line number `line_num` in log `log_name` with the
+  content of `edit`.
+
+  Returns {:ok, path_to_file} if successful.
+  """
+  @doc since: "June 9th, 2019"
+  def edit_daily_log(log_name \\ nil, line_num, edit) do
+    {:ok, path} = make_daily_log(log_name)
+    {:ok, contents} = File.read(path)
+
+    contents
+    |> String.split("\n")
+    |> List.replace_at((line_num-1), String.trim_trailing(edit))
+    |> Enum.map(fn x -> x <> "\n" end)
+    |> (&(if Enum.at(&1, -1) == "\n", do: Enum.drop(&1, -1), else: &1)).() # HACK
+    |> Enum.into(File.stream!(path))
+
+    {:ok, path}
   end
 
   # Grabs input from the terminal one line at a time and terminates
@@ -104,5 +145,21 @@ defmodule Clijo.JournalManager do
     File.mkdir_p(month_path)
 
     {:ok, month_path}
+  end
+
+  # Returns the full path of the given log `log_name`.
+  defp get_log_path(log_name) do
+    {:ok, path} = generate_directory()
+    date = Date.utc_today()
+    day = Integer.to_string(date.day)
+
+    log_path =
+      if is_nil(log_name) do
+        Path.join(path, day) <> ".md"
+      else
+        Path.join(path, log_name) <> ".md"
+      end
+
+    {:ok, log_path}
   end
 end
