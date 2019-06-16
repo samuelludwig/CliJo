3f018d7d3bbf8fb9cdb534474f5d4dd25e8c672f
samuelludwig <samuelludwig74@yahoo.com>
2019-06-16 08:55:40 -0400

    Implement bullet-prefix definition and editing functionality.

--- lib/clijo/config_manager.ex
+++ lib/clijo/config_manager.ex
@@ -5,6 +5,10 @@ defmodule Clijo.ConfigManager do
   """
   @moduledoc since: "June 7th, 2019"
 
+  # TODO Need to clean up this module, there is a lot of repetition that
+  # can be cut down, and a lot of unnecessary instructions being
+  # performed. 
+
   @user_config_path "./config/user_config.json"
 
   @doc """
@@ -16,10 +20,11 @@ defmodule Clijo.ConfigManager do
   @doc since: "June 6th, 2019"
   def set_home_directory(path) do
     if File.exists?(path) do
+      {:ok, user_config} = get_user_config()
       path = Path.expand(path)
       write_to_file = &File.write!(@user_config_path, &1)
       # Update settings map and write to file
-      %{get_user_config() | "home_directory" => path}
+      %{user_config | "home_directory" => path}
       |> Jason.encode!()
       |> write_to_file.()
 
@@ -39,18 +44,88 @@ defmodule Clijo.ConfigManager do
     |> Map.get("home_directory")
   end
 
-  def define_prefixes() do
-    # TODO
+  @doc """
+  Adds and/or edits existing defined prefixes in `user_config.json`
+
+  Returns `{:ok, map_of_prefixes}` if successful.
+  """
+  @doc since: "June 16th, 2019"
+  def define_prefixes(item_prefix_map \\ %{}) do
+	{:ok, user_config} = get_user_config()
+    {:ok, original_prefixes} = get_prefixes()
+
+    new_prefixes = Map.merge(original_prefixes, item_prefix_map)
+    write_to_file = &File.write!(@user_config_path, &1)
+    # Update settings map and write to file
+    %{user_config | "prefixes" => new_prefixes}
+    |> Jason.encode!()
+	|> write_to_file.()
+
+	{:ok, new_prefixes}
   end
 
+  @doc """
+  Deletes the specified prefix of `item` in `user_config.json`.
+
+  Returns `{:ok, map_of_prefixes}` if successful.
+  Will return `{:error, "prefix: 'item' does not exist."}` if the item
+  can't be found in the prefixes map.
+  """
+  @doc since: "June 16th, 2019"
+  def delete_prefix(item) do
+	{:ok, user_config} = get_user_config()
+	{:ok, original_prefixes} = get_prefixes()
+
+	if Map.has_key?(original_prefixes, item) do
+		new_prefixes = Map.delete(original_prefixes, item)
+		write_to_file = &File.write!(@user_config_path, &1)
+		# Update settings map and write to file
+   		%{user_config | "prefixes" => new_prefixes}
+	    |> Jason.encode!()
+  		|> write_to_file.()
+    
+	   	{:ok, new_prefixes}
+	else
+		{:error, "prefix: #{item} does not exist."}
+	end
+  end
+
+  @doc """
+  Gets the `"prefixes"` map in the decoded `user_config.json` file.
+
+  Returns `{:ok, map_of_prefixes}` if successful.
+  """
+  @doc since: "June 16th, 2019"
+  def get_prefixes() do
+	{:ok, user_config} = get_user_config()
+  	
+	user_config
+	|> Map.fetch("prefixes")
+  end
+
+  @doc """
+  Gets the specific prefix of `item` found in `user_config.json`.
+
+  Returns `{:ok, item_prefix}` is successful.
+  """
+  @doc since: "June 16th, 2019"
   def get_prefix(item) do
-    # TODO
+  	{:ok, prefixes} = get_prefixes()
+
+  	prefixes
+    |> Map.fetch(item)
   end
 
-  # Returns a map of the settings defined in `user_config.json`
-  defp get_user_config() do
+  @doc """
+  Gets  a map of the settings defined in `user_config.json`.
+
+  Returns `{:ok, user_config_map}` if successful.
+  """
+  @doc since: "June 16th, 2019"
+  def get_user_config() do
+    {:ok,
     @user_config_path
     |> File.read!()
-    |> Jason.decode!()
+    |> Jason.decode!()}
   end
 end
